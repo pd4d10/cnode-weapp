1d95db2aefc0095e0ac568269dfce9519deddf67
pd4d10 <pd4d10@gmail.com>
2017-04-25 19:02:05 +0800

    Add index page (for topic list)

--- pages/index/index.js
+++ pages/index/index.js
@@ -21,26 +21,60 @@
 //index.js
 //获取应用实例
 var app = getApp()
+
+function fetchTopics(page = 1) {
+  return new Promise((resolve, reject) => {
+    wx.request({
+      url: `https://cnodejs.org/api/v1/topics?tab=all&limit=20&page=${page}`,
+      success(res) {
+        resolve(res.data.data)
+      },
+      fail(err) {
+        reject(err)
+      }
+    })
+  })
+}
+
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {}
+    isLoading: false,
+    page: 1,
+    topics: []
   },
   //事件处理函数
-  bindViewTap: function() {
+  bindViewTap: function () {
     wx.navigateTo({
       url: '../logs/logs'
     })
   },
-  onLoad: function () {
-    console.log('onLoad')
-    var that = this
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
-      //更新数据
-      that.setData({
-        userInfo:userInfo
-      })
+  loadMore(e) {
+    if (this.data.isLoading) {
+      return
+    }
+
+    this.setData({
+      isLoading: true
     })
+
+    fetchTopics(this.data.page + 1)
+      .then(topics => {
+        this.setData({
+          isLoading: false,
+          page: this.data.page + 1,
+          topics: [
+            ...this.data.topics,
+            ...topics,
+          ]
+        })
+      })
+  },
+  onLoad() {
+    fetchTopics()
+      .then(topics => {
+        this.setData({
+          topics: topics
+        })
+      })
   }
 })
