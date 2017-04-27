3b3976c7f7d47dad296701540cf0da51d2c1aeec
pd4d10 <pd4d10@gmail.com>
2017-04-27 22:23:29 +0800

    Add pull down event on message page

--- pages/message/message.js
+++ pages/message/message.js
@@ -1,36 +1,44 @@
-import { getToken } from '../../utils'
+import { getToken } from "../../utils";
 
 Page({
   data: {
     messages: [],
-    verified: false,
+    verified: false
   },
   onReady(options) {
-    this.login()
+    this.getMessage()
   },
-  login() {
-    getToken((token) => {
+  onPullDownRefresh() {
+    this.getMessage(true)
+  },
+  getMessage(isRefresh = false) {
+    getToken(token => {
       this.setData({
-        verified: true,
-      })
+        verified: true
+      });
 
       wx.requestCNode({
         url: `/messages?accesstoken=${token}`,
-        success: (res) => {
-          const { data } = res.data
+        success: res => {
+          const { data } = res.data;
           this.setData({
-            messages: [
-              ...data.hasnot_read_messages,
-              ...data.has_read_messages,
-            ]
-          })
+            messages: [...data.hasnot_read_messages, ...data.has_read_messages]
+          });
+          if (isRefresh) {
+            wx.showToast({
+              title: '更新成功'
+            })
+          }
         },
         fail(err) {
           wx.showToast({
-            title: '获取消息失败'
-          })
+            title: "获取消息失败"
+          });
+        },
+        complete() {
+          wx.stopPullDownRefresh()
         }
-      })
-    })
+      });
+    });
   },
-})
+});
